0f13f482f80576981e3a7a5193bbe31348edd3ee
samuelgiorno <samuelgiorno@gmail.com>
2025-04-21 16:09:57 +0200

    first commit desde visual

---
+++ .learn/exercises/11-homework/README.md
@@ -0,0 +1,25 @@
+# `11` Homework 
+
+## 📝 Instructions:
+
+1. Please write the tests and implementation for the two remaining functions: `fromDollarToYen` and `fromYenToPound`
+2. Please note that you will need to look for the equivalent values ​​between USD -> JPY and JPY -> GBP
+
+| Function name     | Input             | Output                            |
+| ----------------- | ----------------- | ----------------------------------|
+| fromDollarToYen   | a Dollar amount   | equivalent amount in Japan YEN    |
+| fromEuroToDollar  | a Euro amount     | equivalent amount in US Dollar    |
+| fromYenToPound    | a Yen amount      | equivalent amount in British Pound|
+
+## 💡 Hint:
+
++ Remember, these are the currency conversion values:
+
+```js
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+```
---
+++ app.js
@@ -0,0 +1,43 @@
+// Esta es mi función que suma dos números
+//const sum = (a,b) => {
+  //  return a + b
+//}
+
+// Solo un registro en consola para nosotros
+//console.log(sum(7,3))
+
+// Exporta la función para usarla en otros archivos 
+// (similar a la palabra clave "export" cuando se usa webpack)
+//module.exports = { sum };
+
+// one euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // Japan yen
+    "USD": 1.07, // US dollar
+    "GBP": 0.87, // British pound
+};
+
+// Convierte una de dólares a yenes
+function fromDollarToYen(dollars) {
+    if (typeof dollars !== "number" || dollars < 0) return 0;
+    const euros = dollars / oneEuroIs["USD"];
+    const yen = euros * oneEuroIs["JPY"];
+    return parseFloat(yen.toFixed(3));
+}
+
+//Convierte de euros a dólares.
+function fromEuroToDollar(euros) {
+    if (typeof euros !== "number" || euros < 0) return 0;
+    const dollars = euros * oneEuroIs["USD"];
+    return parseFloat(dollars.toFixed(3));
+}
+
+//Convierte de yenes a libras esterlinas.
+function fromYenToPound(yen) {
+    if (typeof yen !== "number" || yen < 0) return 0;
+    const euros = yen / oneEuroIs["JPY"];
+    const pounds = euros * oneEuroIs["GBP"];
+    return parseFloat(pounds.toFixed(3));
+}
+
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound };
